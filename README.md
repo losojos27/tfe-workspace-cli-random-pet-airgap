a0afa3e61c9019cf56121bd26ff5ddb915bd308e
Lee Owensby <lee.owensby@gmail.com>
2022-06-06 09:48:10 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# tfe-workspace-cli-random-pet
---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  cloud {
+    organization = "lo-petgrackle"
+
+    workspaces {
+      name = "cli-random-pet"
+    }
+  }
+}
+
+variable "pet_name_length" {
+  type        = number
+  description = "Number of words in pet name"
+  default     = 2
+}
+
+variable "animal_name_length" {
+  type        = number
+  description = "Number of words in animal name"
+  default     = 3
+}
+
+resource "random_pet" "pet_name" {
+  length = var.pet_name_length
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
+
+resource "random_pet" "animal_name" {
+  length = var.animal_name_length
+}
+
+output "animal_name" {
+  value = random_pet.animal_name.id
+}
